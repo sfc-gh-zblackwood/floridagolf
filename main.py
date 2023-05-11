1ccb456ad55269a6bf9bbd4b636ae2abd6e35cfe
markusfurst <68014045+markusfurst@users.noreply.github.com>
2023-05-11 11:14:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if st.sidebar.checkbox('Log in'):
     time.sleep(1)
     container.empty()
     
-    df=pd.read_csv('forcedecks-test-export-05_05_2023.csv', parse_dates=["Date"])
+    df=pd.read_csv('forcedecks-test-export-05_11_2023.csv', parse_dates=["Date"])
 
     #remove columns and change date
     df_updated=df.drop(['ExternalId','Test Type','Time','BW [KG]','Reps','Tags','Additional Load [lb]'], axis=1)
